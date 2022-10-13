c37c23a97419a3311c45d983d302bd2ae1dd2885
killsen <killsen@qq.com>
2022-10-13 18:24:35 +0800

    完善 install

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -28,29 +28,18 @@ function install( $author_lib_ver ) {
     $file   = "$path/$ver.zip"
     $temp   = "$path/$ver"
 
-    try {
-        $conf = Get-Content "$root/.openrestyrc" | ConvertFrom-JSON
-    } catch { }
-
-    if (-not $conf) { $conf = @{} }
-
-    if (-not $conf.lua_resty_libs) {
-        $conf | Add-Member "lua_resty_libs" @{ "$author/$lib" = "$ver" } -Force
-    } else {
-        $conf.lua_resty_libs | Add-Member "$author/$lib" "$ver" -Force
-    }
-
-    $conf | ConvertTo-Json | Set-Content "$root/.openrestyrc"
-
     if ( -not (Test-Path $path) ) {
         New-Item -Path $path -ItemType Directory | Out-Null        # 创建目录
     }
 
-    if (-not (Test-Path $file)) {
-        try {
-            wget.exe "$url" -O "$file"                              # wget下载
-        } catch {
+    if ( -not (Test-Path $file) ) {
+        try{
+            # wget.exe "$url" -O "$file"                            # wget下载
             Invoke-WebRequest -Uri $url -OutFile $file              # 下载文件
+        } catch {
+            Write-Host "下载文件失败: " -ForegroundColor Yellow -NoNewline
+            Write-Host "$url" -ForegroundColor Red
+            return
         }
     }
 
@@ -58,16 +47,41 @@ function install( $author_lib_ver ) {
         Remove-Item -Path $temp -Recurse -Force -ErrorAction Stop   # 删除临时目录
     }
 
-    try {
-        7z.exe x "$file" -o"$temp" -y -aoa  | Out-Null              # 7zip解压
-    } catch {
+    try{
+        # 7z.exe x "$file" -o"$temp" -y -aoa  | Out-Null            # 7zip解压
         Expand-Archive -Path $file -DestinationPath $temp -Force    # 解压文件
+    } catch {
+        Write-Host "解压文件失败: " -ForegroundColor Yellow -NoNewline
+        Write-Host "$file" -ForegroundColor Red
+        return
     }
 
     # 找到 lib/resty 目录并拷贝文件到 nginx/resty 目录
     $resty = get_resty_path $temp
-    if ($resty) {
-        Copy-Item -Path $resty/* -Destination $root/nginx/resty -Recurse -Force
+    if (-not $resty) { return }
+
+    Copy-Item -Path $resty/* -Destination $root/nginx/resty -Recurse -Force
+
+    try {
+        $conf = Get-Content "$root/.openrestyrc" | ConvertFrom-JSON
+    } catch { }
+
+    if (-not $conf) { $conf = @{} }
+
+    $old_ver = $conf.lua_resty_libs.("$author/$lib")
+
+    if ($old_ver -eq "$ver") { return }  # 版本一致退出
+
+    if ( -not $conf.lua_resty_libs ) {
+        $conf | Add-Member "lua_resty_libs" @{ "$author/$lib" = "$ver" } -Force
+
+    } elseif ( $old_ver ) {
+        $conf.lua_resty_libs.("$author/$lib") = "$ver"
+
+    } else {
+        $conf.lua_resty_libs | Add-Member "$author/$lib" "$ver" -Force
     }
+
+    $conf | ConvertTo-Json | Set-Content "$root/.openrestyrc"
 
 }
