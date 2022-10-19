9a3a153cb8352b61bdc730b98b17bc183329d8b0
killsen <killsen@qq.com>
2022-10-19 11:53:02 +0800

    安装 mingw 新增支持 ver 及 arch

--- scripts/install_mingw.ps1
+++ scripts/install_mingw.ps1
@@ -5,28 +5,40 @@ function install_mingw() {
 
     $root = get_root_path
     $orpm = get_orpm_path
+    $conf = get_orpm_conf
 
     if (-not $root) {
         Write-Host ".orpmrc 文件不存在" -ForegroundColor Red
         return
     }
 
-    $url  = "https://github.com/niXman/mingw-builds-binaries/releases/download/12.2.0-rt_v10-rev0/i686-12.2.0-release-posix-dwarf-rt_v10-rev0.7z"
-    $file = "$orpm/mingw/mingw32.7z"
     $path = "$orpm/mingw"
-
-    $mingw32_bin = "$path/mingw32/bin"
-
-    if (Test-Path "$mingw32_bin/gcc.exe") {
-        return $mingw32_bin
-    }
-
     make_path $path
 
-    # 下载文件并解压
-    $ok = download_expand $url $file $path
-    if (-not $ok) { return }
+    $ver = $conf.mingw_ver
+    if (-not $ver) { $ver = "12.2.0" }
 
-    return $mingw32_bin
+    $url = "https://github.com/niXman/mingw-builds-binaries/releases/download/$ver-rt_v10-rev0/"
+
+    if ($conf.arch -eq "64bit" -or $conf.arch -eq "64") {
+        $url += "x86_64-$ver-release-posix-seh-rt_v10-rev0.7z"
+        $file = "$path/mingw64.7z"
+        $mingw_bin = "$path/mingw64/bin"
+    } else {
+        $url += "i686-$ver-release-posix-dwarf-rt_v10-rev0.7z"
+        $file = "$path/mingw32.7z"
+        $mingw_bin = "$path/mingw32/bin"
+    }
+
+    $gcc_exe = "$mingw_bin/gcc.exe"
+    if (-not (Test-Path $gcc_exe)) {
+        # 下载文件并解压
+        download_expand $url $file $path
+        if (-not (Test-Path $gcc_exe)) {
+            return
+        }
+    }
+
+    return $mingw_bin
 
 }
