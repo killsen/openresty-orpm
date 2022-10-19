ac9e4ae1eefde5c6463ae5837602f6b357fa25b8
killsen <killsen@qq.com>
2022-10-19 11:57:16 +0800

    完善安装过程信息输出

--- scripts/install_all.ps1
+++ scripts/install_all.ps1
@@ -7,23 +7,18 @@
 
 $root = get_root_path
 $orpm = get_orpm_path
+$conf = get_orpm_conf
 
 Clear-Host
 Write-Host
 
 if ($root) {
-    Write-Host "workspace: $root" -ForegroundColor Red
-    Write-Host "orpm home: $orpm" -ForegroundColor Red
+    Write-Host "workspace: " -ForegroundColor Yellow -NoNewline
+    Write-Host "$root"       -ForegroundColor Blue
+    Write-Host "orpm home: " -ForegroundColor Yellow -NoNewline
+    Write-Host "$orpm"       -ForegroundColor Blue
 } else {
     Write-Host ".orpmrc 文件不存在" -ForegroundColor Red
-    Write-Host
-    return
-}
-
-try {
-    $conf = Get-Content "$root/.orpmrc" | ConvertFrom-JSON
-} catch {
-    Write-Host ".orpmrc 文件读取失败" -ForegroundColor Red
     Write-Host
     return
 }
@@ -32,25 +27,25 @@ try {
 Get-Process -Name "nginx*" | Stop-Process -PassThru
 
 Write-Host
-Write-Host "install openresty" -ForegroundColor Yellow
+Write-Host "install openresty: " -ForegroundColor Yellow
 $openresty_win32 = install_openresty
 if (-not $openresty_win32) { return }
 Write-Host $openresty_win32 -ForegroundColor Blue
 
 Write-Host
-Write-Host "install mingw" -ForegroundColor Yellow
+Write-Host "install mingw: " -ForegroundColor Yellow
 $mingw32_bin = install_mingw
 if (-not $mingw32_bin) { return }
 Write-Host $mingw32_bin -ForegroundColor Blue
 
 Write-Host
-Write-Host "install luarocks" -ForegroundColor Yellow
+Write-Host "install luarocks: " -ForegroundColor Yellow
 $luarocks_exe = install_luarocks
 if (-not $luarocks_exe) { return }
 Write-Host $luarocks_exe -ForegroundColor Blue
 
 Write-Host
-Write-Host "install libs" -ForegroundColor Yellow
+Write-Host "install libs: " -ForegroundColor Yellow
 Write-Host "-------------------------------------------------"
 
 if (-not $conf.libs) {
